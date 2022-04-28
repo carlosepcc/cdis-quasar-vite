f0b15c4e09ae1bfc6c5968eec1f93a77c69e1f32
Carlos Enrique <carlosepcardenas@gmail.com>
2022-04-28 10:24:46 -0400

    Deleted yarn.lock

---
+++ src/components/BaseForm.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-dialog persistent position="top">
+    <q-card>
+      <q-card-section class="text-h7 text-uppercase text-weight-light"
+        >{{
+          denunciaObject.id ? "Modificar" : "Nueva"
+        }}
+        Denuncia</q-card-section
+      >
+      <q-separator />
+      <q-card-section>
+        <q-form ref="formulario" @submit="onSubmit" @reset="onReset">
+          <slot></slot>
+          <q-separator class="q-mb-sm q-mt-md" />
+
+          <div class="q-gutter-sm">
+            <q-btn-group outline spread clas="full-width q-mt-md">
+              <q-btn
+                :size="state.dense ? 'sm' : 'md'"
+                label="Restablecer"
+                type="reset"
+                flat
+                color="negative"
+              />
+              <q-btn
+                :size="state.dense ? 'sm' : 'md'"
+                label="Cancelar"
+                flat
+                @click="$emit('closeForm')"
+              />
+            </q-btn-group>
+            <q-btn
+              push
+              :size="state.dense ? 'sm' : 'md'"
+              class="full-width"
+              icon="r_save"
+              label="Guardar"
+              type="submit"
+              color="primary"
+            />
+          </div>
+        </q-form>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+<script setup>
+import { ref, inject } from "vue";
+import { guardar } from "src/composables/useAPI.js";
+import state from "src/composables/useState.js";
+
+//DOM
+const formulario = ref();
+
+//COMPONENT
+const emits = defineEmits(["closeForm"]);
+const url = inject("denunciaUrl");
+
+//STATE
+const denunciasArr = inject("denunciasArr");
+
+const denunciaObject = inject("denunciaObject");
+
+//SUBMIT
+function onSubmit() {
+  guardar(denunciaObject.value, denunciasArr, url);
+  onReset();
+}
+
+//RESET FORM
+function onReset() {
+  //Reset fields
+  denunciaObject.value = null;
+  formulario.value.resetValidation();
+}
+</script>
