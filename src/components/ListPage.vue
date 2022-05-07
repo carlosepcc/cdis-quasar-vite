f754ac6bb45bb387738c7cb919b9f3f8d3efe053
Carlos Enrique <carlosepcardenas@gmail.com>
2022-05-07 10:24:39 -0400

    fix(ui): Table buttons

--- src/components/ListPage.vue
+++ src/components/ListPage.vue
@@ -3,73 +3,73 @@
     <div class="q-gutter-sm row items-center">
       <!-- TABLE / GRID -->
       <q-btn-toggle
-        title="Modo de presentación (Tabla o Rejilla)"
         v-model="isTableGrid"
-        size="sm"
-        toggle-color="primary"
         :options="[
           { value: false, slot: 'tableview' },
           { value: true, slot: 'gridview' },
         ]"
+        size="sm"
+        title="Modo de presentación (Tabla o Rejilla)"
+        toggle-color="primary"
       >
         <template v-slot:tableview>
-          <q-icon name="r_table_chart" />
+          <q-icon name="r_table_chart"/>
         </template>
 
         <template v-slot:gridview>
-          <q-icon name="r_grid_view" />
+          <q-icon name="r_grid_view"/>
         </template>
       </q-btn-toggle>
 
       <!-- DENSE / NORMAL -->
       <q-btn-toggle
-        title="Densidad (Normal o Densa. Sólo en vista de tabla)"
         v-model="isTableDense"
-        size="sm"
-        class="q-mx-sm"
-        toggle-color="primary"
         :options="[
           { value: false, slot: 'normal' },
           { value: true, slot: 'dense' },
         ]"
+        class="q-mx-sm"
+        size="sm"
+        title="Densidad (Normal o Densa. Sólo en vista de tabla)"
+        toggle-color="primary"
       >
         <template v-slot:normal>
           <q-icon name="r_table_rows"/>
-             </template>
+        </template>
 
         <template v-slot:dense>
-          <q-icon name="view_headline" />
+          <q-icon name="view_headline"/>
         </template>
       </q-btn-toggle>
     </div>
     <q-table
-      :class="tableClass"
-      :title="heading"
-      :rows="rows"
-      :columns="columns"
-      :row-key="rowKey"
-      :dense="isTableDense"
-      :grid="isTableGrid"
-      grid-header
-      :hide-header="isTableGrid"
-      :fullscreen="isTableFullscreen"
-      rows-per-page-label="Filas por página"
-      :filter="filter"
-      selection="multiple"
       v-model:selected="selected"
-      separator="vertical"
+      :class="tableClass"
+      :columns="columns"
+      :dense="isTableDense"
+      :filter="filter"
+      :fullscreen="isTableFullscreen"
+      :grid="isTableGrid"
+      :hide-header="isTableGrid"
       :pagination="{ rowsPerPage: 20 }"
+      :row-key="rowKey"
+      :rows="rows"
+      :title="heading"
+      grid-header
+      rows-per-page-label="Filas por página"
+      selection="multiple"
+      separator="vertical"
     >
       <template v-slot:item="props">
         <div
-          class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
           :style="props.selected ? 'transition:.5s;transform: scale(0.95);' : ''"
+          class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"
         >
           <q-card :class="props.selected ? 'bg-grey-2' : ''">
             <q-card-section>
-              <q-checkbox dense v-model="props.selected" :label="props.row.name" />
+              <q-checkbox v-model="props.selected" :label="props.row.name" dense/>
             </q-card-section>
-            <q-separator />
+            <q-separator/>
             <q-list dense>
               <q-item v-for="col in props.cols.filter(col => col.name !== 'desc')" :key="col.name">
                 <q-item-section>
@@ -87,120 +87,122 @@
       <!-- :flat="!isTableGrid" -->
       <!-- TODO :loading="loading" -->
       <template v-slot:top-left>
-        <div class="q-gutter-sm row">
-          <!-- FILTER -->
-          <q-input
-            borderless
-            bottom-slots
-            v-model="filter"
-            label="Filtrar"
-            placeholder="Escriba para filtrar.."
-            :dense="s.dense"
-            hide-bottom-space
-            title="Filtrar los elementos de la tabla por coincidencia de texto"
-          >
-            <template v-slot:prepend>
-              <q-icon name="r_search" />
-            </template>
-            <template v-slot:append>
-              <q-icon
-                v-show="filter"
-                name="r_close"
-                @click="filter = ''"
-                class="cursor-pointer"
-              />
-            </template>
-          </q-input>
-
+        <div class="row">
           <!-- NUEVA ENTRADA -->
           <q-btn
-            flat
-            v-show="isTableFullscreen || $q.screen.gt.xs"
             v-if="
               !(
                 heading == 'Reportes técnicos' &&
                 state.loggedUser.roles[0] == 'Coordinador_de_calidad'
               )
             "
-            icon="add"
+            v-show="isTableFullscreen || $q.screen.gt.xs"
             :dense="s.dense"
+            flat
+            icon="add"
             label="Nueva"
-            title="Añadir una nueva entrada en la tabla"
             no-caps
+            title="Añadir una nueva entrada en la tabla"
             @click="$emit('openForm')"
           />
 
           <!-- ELIMINAR SELECCIÓN-->
           <q-btn
+            :dense="s.dense"
             :disabled="selected.length == 0"
+            flat
             icon="delete"
             label="Eliminar selección"
             no-caps
             text-color="negative"
-            flat
             @click="$emit('deleteRows', selected)"
-            :dense="s.dense"
           />
+
+          <!-- FILTER -->
+          <q-input
+            v-model="filter"
+            :dense="s.dense"
+            borderless
+            bottom-slots
+            hide-bottom-space
+            label="Filtrar"
+            placeholder="Escriba para filtrar.."
+            title="Filtrar los elementos de la tabla por coincidencia de texto"
+          >
+            <template v-slot:prepend>
+              <q-icon name="r_search"/>
+            </template>
+            <template v-slot:append>
+              <q-icon
+                v-show="filter"
+                class="cursor-pointer"
+                name="backspace"
+                @click="filter = ''"
+              />
+            </template>
+          </q-input>
         </div>
       </template>
 
       <template v-slot:top-right>
-        <q-btn flat round icon="refresh" @click="$emit('updateList')" title="Actualizar datos"/>
+        <!-- Refrescar-->
+        <q-btn flat icon="refresh" round title="Actualizar datos" @click="$emit('updateList')"/>
         <!-- FULLSCREEN -->
         <q-toggle
-          :dense="s.dense"
-          size="lg"
-          icon="r_fullscreen"
           v-model="isTableFullscreen"
+          :dense="s.dense"
+          icon="r_fullscreen"
+          size="lg"
           title="Pantalla completa"
         />
       </template>
 
       <template v-slot:body="props">
         <q-tr
-          cursor-pointer
           :props="props"
-          title="Haga click para modificar esta entrada"
-          clickable
           class="cursor-pointer"
+          clickable
+          cursor-pointer
+          title="Haga click para ver o modificar esta entrada"
           @click="$emit('openForm', props.row)"
         >
           <q-td auto-width>
-            <q-checkbox :dense="isTableDense" v-model="props.selected" />
+            <q-checkbox v-model="props.selected" :dense="isTableDense"/>
             <!-- TODO: MODIFY -->
             <q-btn
+              :dense="isTableDense"
+              flat
+              icon="edit"
+              round
               size="sm"
               text-color="accent"
-              flat
-              round
-              :dense="isTableDense"
               @click.stop="$emit('openForm', props.row)"
-              icon="edit"
             />
             <q-btn
+              :dense="isTableDense"
+              flat
+              icon="delete"
+              round
               size="sm"
               text-color="negative"
-              flat
-              round
-              :dense="isTableDense"
               @click.stop="$emit('deleteRows', [props.row])"
-              icon="delete"
             />
           </q-td>
           <q-td v-for="col in props.cols" :key="col.name" :props="props">{{
-            col.value
-          }}</q-td>
+              col.value
+            }}
+          </q-td>
         </q-tr>
       </template>
     </q-table>
 
     <q-page-sticky :offset="[18, 18]" class="lt-sm">
       <q-btn
-        position="bottom-right"
+        color="accent"
         fab
         icon="add"
+        position="bottom-right"
         title="Nueva entrada"
-        color="accent"
         @click="$emit('openForm')"
       />
     </q-page-sticky>
@@ -208,10 +210,11 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import {ref} from "vue";
 import state from "src/composables/useState";
 
-import { useQuasar } from "quasar";
+import {useQuasar} from "quasar";
+
 const $q = useQuasar();
 
 var s = state.value;
@@ -227,7 +230,7 @@ const props = defineProps({
     default: "id",
   },
 });
-const emit = defineEmits(["openForm", "deleteRows","updateList"]);
+const emit = defineEmits(["openForm", "deleteRows", "updateList"]);
 
 // FILTRAR
 const filter = ref("");
--- src/router/routes.js
+++ src/router/routes.js
@@ -4,9 +4,9 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [
       {name: "Inicio", path: "", component: () => import("pages/PageIndex.vue")},
-      {name: "Comisiones Disciplinarias", path: "/comisiones", component: () => import("pages/lists/PageComisiones.vue"),},
-      {name: "Denuncias", path: "/denuncias", component: () => import("pages/lists/PageDenuncias.vue")},
-      {name: "Usuarios", path: "/users", component: () => import("pages/lists/PageUsers.vue")},
+      {name: "Comisiones Disciplinarias", path: "/comisiones", component: () => import("pages/ListPages/PageComisiones.vue"),},
+      {name: "Denuncias", path: "/denuncias", component: () => import("pages/ListPages/PageDenuncias.vue")},
+      {name: "Usuarios", path: "/users", component: () => import("pages/ListPages/PageUsers.vue")},
       {name: "Ayuda", path: "/help", component: () => import("pages/PageHelp.vue")},
       {name: "Acerca de", path: "/about", component: () => import("pages/PageAbout.vue")},
       {name: "Ajustes", path: "/settings", component: () => import("pages/PageSettings.vue")},
