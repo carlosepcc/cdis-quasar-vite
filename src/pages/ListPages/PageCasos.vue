9487d4941fd4776ff0e86c0147b4c8fd2db0437a
Carlos Enrique <carlosepcardenas@gmail.com>
2022-06-03 06:39:32 -0400

    fix

--- src/pages/ListPages/PageCasos.vue
+++ src/pages/ListPages/PageCasos.vue
@@ -3,33 +3,42 @@
     <BaseForm
       v-model="showForm"
       v-show="showForm"
-      formTitle="Declaracion"
+      formTitle="Rol"
       @submit="submitFormData"
       @reset="resetFormData"
       @close-form="closeForm"
     >
       <template v-slot:default>
-        <!-- Nombre declaracion -->
+        <!-- Nombre rol -->
         <q-input
-          v-model.trim="declaracionObject.descripcion"
+          v-model.trim="rolObject.nombre"
           :dense="state.dense"
           :rules="[
             (val) => (val && val.length > 0) || 'Por favor, escriba algo',
           ]"
           clearable
           filled
-          autogrow
-          label="Descripcion"
+          label="Nombre"
+          lazy-rules
+        />
+        <q-select
+          v-model="rolObject.permisos"
+          multiple
+          :dense="state.dense"
+          :options="permisosArr"
+          :rules="[val || 'Por favor, seleccione algo']"
+          filled
+          label="Permisos"
           lazy-rules
         />
       </template>
     </BaseForm>
     <ListPage
-      :columns="declaracionFields"
-      :rows="declaracionesArr"
-      heading="Declaraciones"
+      :columns="rolFields"
+      :rows="rolesArr"
+      heading="Roles"
       rowKey="id"
-      @updateList="listarDeclaraciones"
+      @updateList="listarRoles"
       @open-form="(payload) => openForm(payload)"
       @delete-rows="(selectedRows) => deleteTuples(selectedRows)"
     ></ListPage>
@@ -41,30 +50,31 @@ import ListPage from "components/ListPage.vue";
 import BaseForm from "components/BaseForm.vue";
 import listar, { eliminar, guardar } from "src/composables/useAPI.js";
 import state from "src/composables/useState.js";
-const declaracionFields = ref([
+const rolFields = ref([
   {
-    name: "usuario",
+    name: "nombre",
     required: true,
-    label: "Emisor",
+    label: "Nombre",
     align: "left",
-    field: "usuario",
+    field: "nombre",
     sortable: true,
   },
   {
-    name: "descripcion",
+    name: "permisos",
     required: true,
-    label: "Descripcion",
+    label: "Permisos",
     align: "left",
-    field: "descripcion",
+    field: "permisos",
     sortable: true,
   },
 ]);
-const declaracionesArr = ref([]);
-const url = "/declaracion";
+const rolesArr = ref([]);
+const permisosArr = ref(["ROLE_MOD"]);
+const url = "/rol";
 
 //listar
-const listarDeclaraciones = () => listar(declaracionesArr, url);
-// execute on component load listarDeclaraciones();
+const listarRoles = () => listar(rolesArr, url);
+// execute on component load listarRoles();
 
 //form dialog model
 const showForm = ref(false);
@@ -75,24 +85,24 @@ const closeForm = () => {
 };
 
 // MODIFICAR (Abrir formulario con datos del objeto a modificar)
-const declaracionObject = ref({});
+const rolObject = ref({});
 
 //openForm triggered on: Nueva entrada, Modificar
-const openForm = (obj = { permisos: permisosArr.value[0] }) => {
-  declaracionObject.value = obj;
+const openForm = (obj = {permisos:permisosArr.value[0]}) => {
+  rolObject.value = obj;
   showForm.value = true;
 };
 
 //SUBMIT
 function submitFormData() {
-  guardar(declaracionObject.value, declaracionesArr, url);
+  guardar(rolObject.value, rolesArr, url);
 }
 //RESET
 function resetFormData() {
-  declaracionObject.value = null;
+  rolObject.value = null;
 }
 
 // delete tuples by array of objects
 const deleteTuples = (selectedRows = []) =>
-  eliminar(selectedRows, declaracionesArr, url);
+  eliminar(selectedRows, rolesArr, url);
 </script>
